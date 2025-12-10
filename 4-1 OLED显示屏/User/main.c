26af52092eed25b9c332e2f2f90becd61bdebf43
youyuan0-0 <2742035346@qq.com>
2025-12-10 19:08:20 +0800

    4.OLED显示屏

---
+++ 4-1 OLED显示屏/User/main.c
@@ -0,0 +1,28 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+
+int main(void)
+{
+	OLED_Init();
+
+	
+	while(1)
+	{
+//		OLED_ShowChar(1,1,'a');
+//		Delay_ms(200);
+//		OLED_Clear();
+//		OLED_ShowString(1,1,"hello stm32!");
+//		Delay_ms(200);
+//		OLED_Clear();
+//		OLED_ShowNum(1,1,543221,6);
+//		Delay_ms(200);
+//		OLED_Clear();
+//		OLED_ShowSignedNum
+//		OLED_ShowHexNum
+//		OLED_ShowBinNum
+	}
+}
+   
+
+ 
