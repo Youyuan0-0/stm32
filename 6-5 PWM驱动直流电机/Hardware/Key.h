4a602ffd498c228f7b326ed33086bdd63a3746e2
youyuan0-0 <2742035346@qq.com>
2025-12-18 17:50:51 +0800

    6-5 PWM驱动直流电机

---
+++ 6-5 PWM驱动直流电机/Hardware/Key.h
@@ -0,0 +1,8 @@
+#ifndef __KEY_H_
+#define __KEY_H_
+
+void Key_Init(void);
+uint8_t Key_GetNum();
+uint8_t Key_ON();
+
+#endif
---
+++ 6-5 PWM驱动直流电机/Hardware/Motor.c
@@ -0,0 +1,26 @@
+#include "stm32f10x.h"                  // Device header
+#include "PWM.h"
+
+void Motor_Init(){
+	PWM_Init();
+	
+	GPIO_InitTypeDef GPIO_InitStruct;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_5;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
+} 
+
+void Motor_Speed(int8_t Speed){
+	if(Speed >= 0){
+		GPIO_SetBits(GPIOA,GPIO_Pin_4);
+		GPIO_ResetBits(GPIOA, GPIO_Pin_5);
+		PWM_SetCompare3(Speed);
+	}
+	if(Speed < 0){
+		GPIO_ResetBits(GPIOA,GPIO_Pin_4);
+		GPIO_SetBits(GPIOA, GPIO_Pin_5);
+		PWM_SetCompare3(-Speed);
+	}
+	
+}
